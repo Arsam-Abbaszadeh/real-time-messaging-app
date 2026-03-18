630ccca1e96d5d916851c8618bd33fc85690b088
Arsam Abbaszadeh <abbaszadeharsam@gmail.com>
2026-03-18 18:37:22 +1100

    add FKs that ef core missed

---
+++ src/backend/realTimeMessagingWebAppInfra/Persistence/Migrations/20260318051346_AddMissingChatUserForeignKeys.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace realTimeMessagingWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingChatUserForeignKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_ChatAdminId",
+                table: "Chats",
+                column: "ChatAdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_ChatCreatorId",
+                table: "Chats",
+                column: "ChatCreatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_Users_ChatAdminId",
+                table: "Chats",
+                column: "ChatAdminId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_Users_ChatCreatorId",
+                table: "Chats",
+                column: "ChatCreatorId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_Users_ChatAdminId",
+                table: "Chats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_Users_ChatCreatorId",
+                table: "Chats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chats_ChatAdminId",
+                table: "Chats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chats_ChatCreatorId",
+                table: "Chats");
+        }
+    }
+}
