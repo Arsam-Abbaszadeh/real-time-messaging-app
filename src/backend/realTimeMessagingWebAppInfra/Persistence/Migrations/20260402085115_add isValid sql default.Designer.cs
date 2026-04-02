70973c2c41876e5572fe991e571aa049b7f36418
Arsam Abbaszadeh <abbaszadeharsam@gmail.com>
2026-04-02 20:47:53 +1100

    add migration to add default value of true to isValid column on refreshToken relation. Also remove .ValueGeneratedOnAdd() from USER isDeleted and isValid REFRESHTOKEN

--- src/backend/realTimeMessagingWebAppInfra/Persistence/Migrations/20260402085115_add isValid sql default.Designer.cs
+++ src/backend/realTimeMessagingWebAppInfra/Persistence/Migrations/20260402085115_add isValid sql default.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using realTimeMessagingWebAppInfra.Persistence.Data;
@@ -11,9 +12,11 @@ using realTimeMessagingWebAppInfra.Persistence.Data;
 namespace realTimeMessagingWebApp.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20260402085115_add isValid sql default")]
+    partial class addisValidsqldefault
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -215,6 +218,7 @@ namespace realTimeMessagingWebApp.Migrations
                         .HasColumnType("uuid");
 
                     b.Property<bool>("isValid")
+                        .ValueGeneratedOnAdd()
                         .HasColumnType("boolean")
                         .HasDefaultValue(true);
 
@@ -233,6 +237,7 @@ namespace realTimeMessagingWebApp.Migrations
                         .HasDefaultValueSql("gen_random_uuid()");
 
                     b.Property<bool>("IsDeleted")
+                        .ValueGeneratedOnAdd()
                         .HasColumnType("boolean")
                         .HasDefaultValue(false);
 
---
+++ src/backend/realTimeMessagingWebAppInfra/Persistence/Migrations/20260402085115_add isValid sql default.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace realTimeMessagingWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addisValidsqldefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isValid",
+                table: "RefreshTokens",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isValid",
+                table: "RefreshTokens",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: null); // even though migrations may suggest otherwise, the databse says so
+        }
+    }
+}
